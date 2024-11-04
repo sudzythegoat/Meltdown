06d252434d0b1c2849dbb688f893d0b400d4a454
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-04 08:05:56 -0600

    Update meltdown.py

--- meltdown.py
+++ meltdown.py
@@ -1,15 +1,31 @@
 from colorama import Fore, Style
-
+from requests import get
+import os
 class Color:
     WARNING = Fore.RED
     OK = Fore.GREEN
     DEFAULT = Style.RESET_ALL 
     
 def main():
-    global c
-    c = input("[>] ")
-
+    global cmd
+    cmd = input("[>] ")
+    
 def parse():
-    global c
-    if c.startswith("-p"):
-        p = c[5:]
+    global cmd
+    if cmd.startswith("-g"):
+        url = cmd[3:]
+        counter = 0
+        for _ in range(config.amount):
+            check = get(url)
+            counter += 1
+            os.system("clear")
+            if check.statuscode == "200":
+                print(f"{Color.OK}Sent Requests: {counter}{Color.DEFAULT}")
+            else:
+                print(f"{Color.WARNING}Sent Requests: {counter}{Color.DEFAULT}")
+    elif cmd.startswith("-p"):
+        url = cmd[3:]
+        counter = 0
+        for _ in range(config.amount):
+            os.system(f"ping {url}")
+            print(f"{Color.OK}Sent Requests: {counter}{Color.DEFAULT}")
